33b574e80723e6417fe34bc2e43fcef4c39817b6
Sahil Gidwani <79597840+sahil-gidwani@users.noreply.github.com>
2023-11-07 23:16:56 +0530

    Update pre.py

--- pre.py
+++ pre.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 columns_to_normalize = ['column1', 'column2', 'column3']
 df[columns_to_normalize] = scaler.fit_transform(df[columns_to_normalize])
+
 # Z-Score Normalization
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
@@ -24,11 +25,6 @@ df['placement_score'] = df['placement_score'].fillna(method='bfill')
 df['club_join_date'] = df['club_join_date'].replace(to_replace=np.nan,value='2019')
 df['placement_offer_count']=df['placement_offer_count'].fillna('1')
 
-df['math_score']=df['math_score'].astype('int64')
-df['reading_score']=df['reading_score'].astype('int64')
-df['writing_score']=df['writing_score'].astype('int64')
-df['placement_score']=df['placement_score'].astype('int64')
-df['club_join_date']=df['club_join_date'].astype('int64')
 df['placement_offer_count']=df['placement_offer_count'].astype('int64')
 
 z_scores = np.abs(stats.zscore(data))
